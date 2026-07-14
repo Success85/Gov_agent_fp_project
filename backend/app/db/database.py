c67521e14d352f930e634691b8d9a0d8577faf86
HenrietteIraguha <h.iraguha@alustudent.com>
2026-07-14 15:47:46 +0200

    Updated the databse with  Application and ApplicationData to create_tables import

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -50,6 +50,7 @@ def create_tables():
     from app.models.steps import Step
     from app.models.conversation import Conversation
     from app.models.message import Message
+    from app.models.application import Application, ApplicationData
 
     Base.metadata.create_all(bind=engine)
     logger.info(f"Tables created: {list(Base.metadata.tables.keys())}")
